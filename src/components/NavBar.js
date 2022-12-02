2c2fd56da296bcd73ae08c027490ccc1492bed8d
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2022-12-02 14:36:11 +0100

    Nav Modification

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BsGithub } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const NavBar = () => {
-
-    return (
-        <div className="nav">
-            <h2> Heaven's Blog</h2>
-            <div className="icons">
-                <a href="https://github.com/Heavenlydawn?tab=repositories">
-                    <i class="fa fa-2x fa-github-alt" aria-hidden="true"></i>
-                </a>
-            </div>
+  return (
+    <div className="nav">
+      <h2> Heaven's Blog</h2>
+      <div className="icons flex">
+        <div className="flex">
+        <a href="https://github.com/Heavenlydawn?tab=repositories">
+          <BsGithub  className="mr-10 w-14 h-14"/>
+        </a>
+        <a href="https://twitter.com/GabrielHeavenl1">
+          <BsTwitter className="ml-10 w-14 h-14" />
+        </a>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
